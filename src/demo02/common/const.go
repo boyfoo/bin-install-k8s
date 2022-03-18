7978ae9ebaf4c25840e269cc1c60ffed7d0ad465
zhengxin <zx358317301@qq.com>
2022-03-18 15:27:08 +0800

    feat

---
+++ src/demo02/common/const.go
@@ -0,0 +1,6 @@
+package common
+
+var (
+	Namespace = "default"
+	GoTime    = "2006-01-02 15:03:04"
+)
--- src/demo02/controller/deploy_controller.go
+++ src/demo02/controller/deploy_controller.go
@@ -3,13 +3,16 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 	"net/http"
+	"src/demo02/common"
 )
 
 type DeployController struct {
@@ -18,11 +21,36 @@ type DeployController struct {
 
 // List 获取全部deploy
 func (d *DeployController) List() (*appsv1.DeploymentList, error) {
-	list, err := d.client.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
+	list, err := d.client.AppsV1().Deployments(common.Namespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
 	return list, nil
+}
+
+func (d *DeployController) Show(name string) (*appsv1.Deployment, error) {
+	return d.client.AppsV1().Deployments(common.Namespace).Get(context.Background(), name, metav1.GetOptions{})
+}
+
+func (d *DeployController) getPodsByDeployment(dep *appsv1.Deployment) (*corev1.PodList, error) {
+
+	// 标签选择
+	labels := ""
+	for k, v := range dep.Spec.Selector.MatchLabels {
+		if labels != "" {
+			labels += ","
+		}
+		labels += fmt.Sprintf("%s=%s", k, v)
+	}
+
+	podList, err := d.client.CoreV1().Pods(dep.Namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: labels,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return podList, nil
 }
 
 // CreateForYaml 通过文件创建
@@ -31,7 +59,7 @@ func (d *DeployController) CreateForYaml(path string) (*appsv1.Deployment, error
 	if err != nil {
 		return nil, err
 	}
-	create, err := d.client.AppsV1().Deployments("default").Create(context.TODO(), dep, metav1.CreateOptions{})
+	create, err := d.client.AppsV1().Deployments(common.Namespace).Create(context.TODO(), dep, metav1.CreateOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -44,7 +72,7 @@ func (d *DeployController) DeleteForYaml(path string) error {
 	if err != nil {
 		return err
 	}
-	return d.client.AppsV1().Deployments("default").Delete(context.TODO(), dep.Name, metav1.DeleteOptions{})
+	return d.client.AppsV1().Deployments(common.Namespace).Delete(context.TODO(), dep.Name, metav1.DeleteOptions{})
 }
 
 // 根据文件路径获取json
@@ -77,11 +105,6 @@ func DeployRoute(client *kubernetes.Clientset, r *gin.Engine) {
 		res := []map[string]interface{}{}
 		for _, item := range list.Items {
 
-			images := ""
-			for _, c := range item.Spec.Template.Spec.Containers {
-				images += c.Image
-			}
-
 			res = append(res, map[string]interface{}{
 				"名称": item.Name,
 				"副本数": map[string]interface{}{
@@ -89,10 +112,43 @@ func DeployRoute(client *kubernetes.Clientset, r *gin.Engine) {
 					"可用副本数":  item.Status.AvailableReplicas,
 					"不可用副本数": item.Status.UnavailableReplicas,
 				},
-				"镜像名称": images,
+				"镜像名称": getImages(&item),
 			})
 		}
 		ctx.JSON(200, res)
+	})
+	r.GET("deploy/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		show, err := deployController.Show(name)
+		if err != nil {
+			ctx.JSON(200, err)
+			return
+		}
+
+		// 获取pod
+		podList, err := deployController.getPodsByDeployment(show)
+		if err != nil {
+			ctx.JSON(200, err)
+			return
+		}
+
+		var podsMap []map[string]interface{}
+		for _, pod := range podList.Items {
+			podsMap = append(podsMap, map[string]interface{}{
+				"名称":   pod.Name,
+				"镜像":   getImagesByPod(&pod),
+				"所属节点": pod.Spec.NodeName,
+				"创建时间": pod.CreationTimestamp.Format(common.GoTime),
+			})
+		}
+
+		ctx.JSON(200, map[string]interface{}{
+			"1.名称":   show.Name,
+			"2.命名空间": show.Namespace,
+			"3.镜像":   getImages(show),
+			"4.创建时间": show.CreationTimestamp.Format(common.GoTime),
+			"5.pods": podsMap,
+		})
 	})
 	r.POST("deploy", func(ctx *gin.Context) {
 		path := &struct {
@@ -121,6 +177,65 @@ func DeployRoute(client *kubernetes.Clientset, r *gin.Engine) {
 		}
 		ctx.JSON(http.StatusOK, nil)
 	})
+
+	// 更新副本数
+	r.POST("deploy-scale", func(ctx *gin.Context) {
+		req := &struct {
+			DeploymentName string `json:"deployment_name"`
+			Dec            bool   `json:"dec"`
+		}{}
+		err := ctx.ShouldBindJSON(req)
+		if err != nil {
+			ctx.JSON(http.StatusOK, err)
+			return
+		}
+		// 获取原本的
+		scale, err := deployController.client.AppsV1().Deployments(common.Namespace).GetScale(
+			context.Background(),
+			req.DeploymentName,
+			metav1.GetOptions{},
+		)
+		if err != nil {
+			ctx.JSON(http.StatusOK, err)
+			return
+		}
+
+		if req.Dec {
+			scale.Spec.Replicas--
+		} else {
+			scale.Spec.Replicas++
+		}
+		updateScale, err := deployController.client.AppsV1().Deployments(common.Namespace).UpdateScale(
+			context.Background(),
+			req.DeploymentName,
+			scale,
+			metav1.UpdateOptions{},
+		)
+		if err != nil {
+			ctx.JSON(http.StatusOK, err)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, updateScale)
+	})
+}
+
+// 获取镜像
+func getImages(item *appsv1.Deployment) string {
+	images := ""
+	for _, c := range item.Spec.Template.Spec.Containers {
+		images += c.Image
+	}
+	return images
+}
+
+// 获取pod的镜像
+func getImagesByPod(item *corev1.Pod) string {
+	images := ""
+	for _, c := range item.Spec.Containers {
+		images += c.Image
+	}
+	return images
 }
 
 func NewDeployController(client *kubernetes.Clientset) *DeployController {
